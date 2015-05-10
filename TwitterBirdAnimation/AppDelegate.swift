c2d919c3aac1412763b20d79ddfb0ab593f06d29
Rounak <rounak91@gmail.com>
2015-05-10 10:19:03 +0530

    fixed swift compiler errors and content aspect

--- TwitterBirdAnimation/AppDelegate.swift
+++ TwitterBirdAnimation/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window!.addSubview(imageView)
         self.mask = CALayer()
         self.mask!.contents = UIImage(named: "twitter logo mask")!.CGImage
+        self.mask!.contentsGravity = kCAGravityResizeAspect
         self.mask!.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
         self.mask!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         self.mask!.position = CGPoint(x: imageView.frame.size.width/2, y: imageView.frame.size.height/2)
